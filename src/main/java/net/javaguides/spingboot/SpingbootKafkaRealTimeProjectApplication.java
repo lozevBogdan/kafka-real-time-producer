521c17c018035b46e5423abac38a5e8d848d1ae3
Bogdan Lozev <lozev.bogdan@abv.bg>
2024-07-25 22:03:11 +0300

    Initial commit

---
+++ src/main/java/net/javaguides/spingboot/SpingbootKafkaRealTimeProjectApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootKafkaRealTimeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootKafkaRealTimeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/javaguides/spingboot/SpingbootKafkaRealTimeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootKafkaRealTimeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
