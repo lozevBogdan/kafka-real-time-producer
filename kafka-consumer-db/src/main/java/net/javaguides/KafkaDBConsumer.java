8dc5eb02417797f9cb32012133b985bc24afb63c
Bogdan Lozev <lozev.bogdan@abv.bg>
2024-07-26 21:49:31 +0300

    Kafka DB Consumer Entity and repo

--- kafka-consumer-db/src/main/java/net/javaguides/KafkaDBConsumer.java
+++ kafka-consumer-db/src/main/java/net/javaguides/KafkaDBConsumer.java
@@ -1,7 +1,10 @@
 package net.javaguides;
 
+import net.javaguides.entity.WikimediaData;
+import net.javaguides.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,21 @@ public class KafkaDBConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDBConsumer.class);
 
+    @Autowired
+    private WikimediaDataRepository wikimediaDataRepository;
+
     @KafkaListener(
             topics = "wikimedia_recentchange",
             groupId = "myGroup"
     )
     public void consume(String eventMessage){
-        
+
         LOGGER.info(String.format("Event message received -> %s", eventMessage));
+
+        WikimediaData data = new WikimediaData();
+        data.setWikiEventData(eventMessage);
+
+        wikimediaDataRepository.save(data);
 
     }
 }
---
+++ kafka-consumer-db/src/main/java/net/javaguides/repository/WikimediaDataRepository.java
@@ -0,0 +1,9 @@
+package net.javaguides.repository;
+
+import net.javaguides.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
